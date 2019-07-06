cc493cb893d2a1fb3238927d03edd85b13fac39e
Melodeiro <mr.melodeiro@gmail.com>
2019-07-07 01:35:35 +0300

    Fix jar building

--- desktop/src/com/github/melodeiro/ksnake/desktop/DesktopLauncher.kt
+++ desktop/src/com/github/melodeiro/ksnake/desktop/DesktopLauncher.kt
@@ -1,13 +1,12 @@
+@file:JvmName("DesktopLauncher")
+
 package com.github.melodeiro.ksnake.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.github.melodeiro.ksnake.Game
 
-object DesktopLauncher {
-    @JvmStatic
-    fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration()
-        LwjglApplication(Game(), config)
-    }
+fun main(args: Array<String>) {
+    val config = LwjglApplicationConfiguration()
+    LwjglApplication(Game(), config)
 }
