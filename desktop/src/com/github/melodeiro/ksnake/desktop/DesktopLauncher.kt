941375660efdf92b312bd250d656e091d9c0e6a8
Melodeiro <mr.melodeiro@gmail.com>
2019-07-06 20:16:02 +0300

    Initial commit

---
+++ desktop/src/com/github/melodeiro/ksnake/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.github.melodeiro.ksnake.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.github.melodeiro.ksnake.Game
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
